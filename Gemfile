2c5b6ed4a30b0626a7515353a1cd108dab9056ce
benrmatthews <ben@brightone.org.uk>
2013-05-27 20:45:36 +0100

    Added Devise

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ end
 gem 'jquery-rails'
 gem 'rails_log_stdout',           github: 'heroku/rails_log_stdout'
 gem 'rails3_serve_static_assets', github: 'heroku/rails3_serve_static_assets'
+gem 'devise'
+gem 'protected_attributes'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Frankln::Application.routes.draw do
+
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+
+
+  get "users/new"
+
+  root to: 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
