d16daece80cf4e8f4a294c9ddbb68e96ce73e8b1
benrmatthews <ben@brightone.org.uk>
2013-05-27 18:35:53 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Frankln::Application.config.secret_key_base = '95e294a542c45e2fab16ff82acf4b0b08b811d157c110e190b5bfe181fbbba9388fc57c68c9407c70d17261fa0bc9ef848cc78dce7c702b7cdadd2ec0f41e82a'
